bbba51a02c247df8038b917d53c2ab0668d8010c
avidCutlet <vonwebsterlabajo@gmail.com>
2021-02-09 02:30:15 +0800

    research UI filter fix

--- php/research_pagination.php
+++ php/research_pagination.php
@@ -46,13 +46,80 @@
           echo '';
         } else { ?>
           <!-- Filter changed -->
-                <div class="row">
-                    <!-- br for see list -->
-                    <br>
 
+          <label>See List: </label>
+            <button type="button" class="fa fa-file-text btn btn-outline-primary" data-toggle="modal" data-target="#seeList"></button>
+            <div class="modal fade" tabindex="-1" aria-labelledby="myExtraLargeModalLabel" id="seeList" role="dialog" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-scrollable modal-xl">
+                <!-- modal content -->
+                <div class="modal-content">
+                  <!-- modal header -->
+                  <div class="modal-header">
+                    <h4 class="header-font">Research Study List&ThickSpace;</h4>
+                    <button type="button" onclick="exportFullListPdf()" class="fa fa-download btn btn-outline-primary" data-toggle="tooltip" title="Download research study list"></button>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  </div>
 
+                  <!-- modal and table added -->
+                  <!-- modal body -->
+                  <div class="modal-body">
+                    <div class="table">
+                      <table class="table table-bordered" id="fullListPdf">
+                        <tbody>
+                          <?php if (mysqli_num_rows($fullList) > 0): ?>
+                                                <?php while ($row_fullList = mysqli_fetch_array($fullList)) { ?>
+                                                  <tr class="first-border-top first-border-bottom">
+                                                    <td class="font-weight-bold" colspan="5"><?php echo $row_fullList['Title'] ?></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td class="font-weight-bold  td-style">Author1</td><td><?php echo $row_fullList['Author'] ?></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td class="font-weight-bold td-style">Author2</td><td><?php echo $row_fullList['Author'] ?></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td class="font-weight-bold td-style">Year</td><td><?php echo $row_fullList['Year'] ?></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td class="font-weight-bold td-style">Course</td><td><?php echo $row_fullList['Course'] ?></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td class="font-weight-bold td-style">Adviser</td><td><?php echo $row_fullList['Adviser'] ?></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td class="font-weight-bold td-style">Keywords</td><td><?php echo $row_fullList['Keywords'] ?></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td class="font-weight-bold td-style">Date Uploaded</td><td><?php echo $row_fullList['Upload_Date'] ?></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td class="font-weight-bold td-style">Views</td><td><?php echo $row_fullList['Views'] ?></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td class="font-weight-bold td-style">Downloads</td><td><?php echo $row_fullList['Downloads'] ?></td>
+                                                  </tr>
+                                                  <tr class="last-border-bottom">
+                                                    <td class="font-weight-bold td-style"># of searches</td><td><?php echo $row_fullList['Search'] ?></td>
+                                                  </tr>
+                                                <?php } ?>
+                                              <?php endif ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <!-- modal body -->
+                            </div>
+                            <!-- modal dialog -->
+                        </div>
+                        <!-- modal fade -->
+                    </div>
+          <br>
+          <!-- hr added -->
+          <hr>
+                  <!-- row removed -->
                   <!-- Checkboxes for Adviser added -->
-                  <div class="col">
+                  <!-- col remove -->
+                  <div>
 
                     <div class="mt-4">
                       <label>ADVISER</label>
@@ -61,16 +128,16 @@
 
                     <br>
                       <input type="checkbox" name="filter_checkbox_adviser1" id="filter_checkbox_adviser1">
-                      <label for="filter_checkbox_adviser1" id="filter_label_adviser1">Von, Webster&nbsp;(10)</label>
+                      <label for="filter_checkbox_adviser1" id="filter_label_adviser1">Von, WebsterWebsterWebsterWebster&nbsp;(10)</label>
 
 
                     <br>
                       <input type="checkbox" name="filter_checkbox_adviser2" id="filter_checkbox_adviser2">
-                      <label for="filter_checkbox_adviser2" id="filter_label_adviser2">Santos, Noel&nbsp;(9)</label>
+                      <label for="filter_checkbox_adviser2" id="filter_label_adviser2">Santos, Noel noel noel&nbsp;(9)</label>
 
                     <br>
                       <input type="checkbox" name="filter_checkbox_adviser3" id="filter_checkbox_adviser3">
-                      <label for="filter_checkbox_adviser3" id="filter_label_adviser3">Cruz, Jari&nbsp;(3)</label>
+                      <label for="filter_checkbox_adviser3" id="filter_label_adviser3">Cruz, Jari jari jari jari jari&nbsp;(3)</label>
                     <br>
 
                     
@@ -90,8 +157,9 @@
                   </div>
 
                   <!-- Checkboxes for year added -->
-                  <div class="col-7 ">
-                    <hr class="sm-hide">
+                  <div>
+                    <!-- sm-hide and col remove -->
+                    <hr>
 
                     <label class="mt-4">YEAR</label>
                     <br>
@@ -128,12 +196,10 @@
                   </div>
 
 
-                <!-- row -->
-                </div>
-
                 <!-- Checkboxes for Course added -->
                 <div>
-                  <hr class="sm-hide">
+                  <!-- sm-hide remove -->
+                  <hr>
 
                   <div class="mt-4">
                     <label>COURSE</label>
@@ -166,8 +232,8 @@
 
         <?php } ?>
 
-        <!-- hr added -->
-        <hr class="sm-show">
+        <!-- sm-show remove -->
+        <hr>
 
         <!-- first column -->
       </div>
